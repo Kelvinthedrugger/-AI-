4442a5b5f11b0b3db18fadf39363270271035964
Kelvinthedrugger <890225kelvin@gmail.com>
2022-07-31 03:44:21 +0800

    add items

--- PLURK/plurk_fetch.py
+++ PLURK/plurk_fetch.py
@@ -1,4 +1,29 @@
-# fetch content from plurk website using their public apis
+# fetch content from plurk website using their public APIs
+
+
+"""
+#useful APIs
+
+/APP/FriendsFans/getxxxByOffset: If only you have friends/fans/following
+
+/APP/Profile/getPublicProfile
+    require: user_id; use "search" to get this
+
+/APP/Timeline/getPlurks
+
+/APP/Responses/get: fetch responses, so, be active on social apps!
+    require: plurk_id; needs to hack-around it, play around and try xpath again ...
+
+/APP/PlurkSearch/search: search plurks (not sure what "plurk" means, ~20)
+    require: query after users
+
+/APP/UserSearch/search: search users (~10)
+    require: query after users
+
+
+/APP/Emoticons/get: get emoji's
+
+"""
 
 def main():
     pass
